9fc72ae819c95ef435aa27e8556e7173b1daa54c
timothyis <hello@timothy.is>
2019-10-29 21:46:00 +0000

    Add spacing between header and quickstarts

--- components/quickstarts/quickstarts.js
+++ components/quickstarts/quickstarts.js
@@ -2,8 +2,11 @@ import { List } from '~/components/list'
 
 export default function Quickstarts({ children }) {
   return (
-    <List columnsDesktop={2} columnsTablet={2} columnsMobile={1}>
-      {children}
-    </List>
+    <>
+      <br />
+      <List columnsDesktop={2} columnsTablet={2} columnsMobile={1}>
+        {children}
+      </List>
+    </>
   )
 }
